3c2ed18a855bb4f43eeca416ff7c07838e08b78d
SyedNajiullah <najiullahsyed2@gmail.com>
2026-03-12 13:05:06 +0500

    added admin.py file for registering all classes

--- core/products/admin.py
+++ core/products/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Brand, Category, Product, ProductSize, ProductImage
 
-# Register your models here.
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent')
+
+class ProductSizeInline(admin.TabularInline):
+    model = ProductSize
+    extra = 1
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'brand', 'category', 'price', 'is_on_sale')
+    list_filter = ('brand', 'category', 'is_on_sale')
+    search_fields = ('name', 'sku')
+    inlines = [ProductSizeInline, ProductImageInline]
