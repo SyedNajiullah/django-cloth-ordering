dac0e2912a513caae66996f0e1dd2e578e461f0c
SyedNajiullah <najiullahsyed2@gmail.com>
2026-03-13 10:13:13 +0500

    changed landing hero image

--- core/core/settings.py
+++ core/core/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+if not DEBUG:
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
