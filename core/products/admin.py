06b152d06a306413d9fed5ac0dcf6c9a8d451749
SyedNajiullah <najiullahsyed2@gmail.com>
2026-03-16 11:10:04 +0500

    removed parent from list display from category admin

--- core/products/admin.py
+++ core/products/admin.py
@@ -7,7 +7,7 @@ class BrandAdmin(admin.ModelAdmin):
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'parent')
+    list_display = ('name',)
 
 class ProductSizeInline(admin.TabularInline):
     model = ProductSize
